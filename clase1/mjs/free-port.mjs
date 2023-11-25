46cc2b1ba99459deb5074a0b584f7b40892dc187
Diego Rodriguez <diego25598@gmail.com>
2023-11-25 21:33:05 +0100

    add http server with function to search free ports

--- clase1/mjs/free-port.mjs
+++ clase1/mjs/free-port.mjs
@@ -1,31 +1,24 @@
-import { net } from 'node:net'
+import net from 'node:net'
 
-function findAvailablePort (desiredPort) {
-
+export function findAvailablePort (desiredPort) {
   return new Promise((resolve, reject) => {
-
     const server = net.createServer()
 
     server.listen(desiredPort, () => {
-
       const { port } = server.address()
-
-      server.close(() => { resolve(port) })
-
+      server.close(() => {
+        resolve(port)
+      })
     })
 
     server.on('error', (err) => {
-
       if (err.code === 'EADDRINUSE') {
-
         findAvailablePort(0).then(port => resolve(port))
-
-      } else { reject(err) }
-
+      } else {
+        reject(err)
+      }
     })
-
   })
-  
 }
 
-module.exports = { findAvailablePort }
+// module.exports = { findAvailablePort }
--- clase1/mjs/http.mjs
+++ clase1/mjs/http.mjs
@@ -1,24 +1,17 @@
-import { http } from 'node:http' 
-
-const { findAvailablePort } = require('./free-port.mjs')
+import http from 'node:http' // Protocolo HTTP
+import { findAvailablePort } from './free-port.mjs'
 
 console.log(process.env)
 
 const desiredPort = process.env.PORT ?? 3000
 
 const server = http.createServer((req, res) => {
-    
   console.log('request received')
   res.end('Hola mundo')
-
 })
 
 findAvailablePort(desiredPort).then(port => {
-
   server.listen(port, () => {
-
     console.log(`server listening on port http://localhost:${port}`)
-
   })
-
 })
