99916513674f51dcd0440aa1dac499850c4ee9e8
Diego Rodriguez <diego25598@gmail.com>
2023-11-27 18:22:14 +0100

    add rate default value in movie schema

--- clase3/schemas/movies.js
+++ clase3/schemas/movies.js
@@ -7,12 +7,12 @@ const movieSchema = zod.object({
   }),
   year: zod.number().int().positive().min(1900).max(2024),
   duration: zod.number().int().positive(),
-  rate: zod.number().min(0).max(10),
+  rate: zod.number().min(0).max(10).default(5),
   poster: zod.string().url({
     message: 'Poster must be a valid url'
   }),
   genre: zod.array(
-    zod.enum(['Action', 'Comedy', 'Adventure', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']), {
+    zod.enum(['Action', 'Adventure', 'Comedy', 'Crime', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']), {
       required_error: 'Movie genre is required',
       invalid_type_error: 'Movie genres must be an array of enum genre'
     }
