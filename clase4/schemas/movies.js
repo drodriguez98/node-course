f6580b2e361d9453034cd6d04d09d430fa6cd173
Diego Rodriguez <diego25598@gmail.com>
2023-11-28 00:56:02 +0100

    add movies schema

--- clase4/schemas/movies.js
+++ clase4/schemas/movies.js
@@ -1,32 +1,30 @@
-import zod from 'zod'
+import z from 'zod'
 
-const movieSchema = zod.object({
-
-  title: zod.string({
-
+const movieSchema = z.object({
+  title: z.string({
     invalid_type_error: 'Movie title must be a string',
-    required_error: 'Movie title is required'
-
+    required_error: 'Movie title is required.'
   }),
-
-  year: zod.number().int().positive().min(1900).max(2024),
-  duration: zod.number().int().positive(),
-  rate: zod.number().min(0).max(10).default(5),
-  poster: zod.string().url({ message: 'Poster must be a valid url' }),
-
-  genre: zod.array(
-
-    zod.enum(['Action', 'Adventure', 'Comedy', 'Crime', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']), {
-
-      required_error: 'Movie genre is required',
-      invalid_type_error: 'Movie genres must be an array of enum genre'
-
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(5),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL'
+  }),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+    {
+      required_error: 'Movie genre is required.',
+      invalid_type_error: 'Movie genre must be an array of enum Genre'
     }
-
   )
-
 })
 
-export function validateMovie (input) { return movieSchema.safeParse(input) }
+export function validateMovie (input) {
+  return movieSchema.safeParse(input)
+}
 
-export function validatePartialMovie (input) { return movieSchema.partial().safeParse(input) }
+export function validatePartialMovie (input) {
+  return movieSchema.partial().safeParse(input)
+}
