711d958cfea95777ce858235f94d1aa594807d81
Diego Rodriguez <diego25598@gmail.com>
2023-11-27 18:55:59 +0100

    Add controller to update movies by id

--- clase3/app.js
+++ clase3/app.js
@@ -1,23 +1,26 @@
 const express = require('express')
 const crypto = require('node:crypto')
 const movies = require('./movies.json')
-const { validateMovie } = require('./schemas/movies')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.use(express.json())
-
 app.disable('x-powered-by')
+
+// Obtener todas las películas
 
 app.get('/movies', (req, res) => {
   const { genre } = req.query
+
   if (genre) {
-    const filteredMovies = movies.filter(
-      movie => movie.genre.includes(genre)
-    )
+    const filteredMovies = movies.filter(movie => movie.genre.includes(genre))
     return res.json(filteredMovies)
   }
+
   res.json(movies)
 })
+
+// Obtener una película por su id
 
 app.get('/movies/:id', (req, res) => {
   const { id } = req.params
@@ -26,23 +29,47 @@ app.get('/movies/:id', (req, res) => {
   res.status(404).json({ message: 'Movie not found' })
 })
 
+// Crear una película utilizando validaciones zod
+
 app.post('/movies', (req, res) => {
   const result = validateMovie(req.body)
 
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
+  if (result.error) { return res.status(400).json({ error: JSON.parse(result.error.message) }) }
+
   const newMovie = {
     id: crypto.randomUUID(),
     ...result.data
   }
+
   // Esto no sería rest porque estamos guardando el estado de la aplicación en memoria.
   movies.push(newMovie)
   res.status(201).json(newMovie)
 })
 
+// Actualizar una película utilizando validaciones zod
+
+app.patch('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) { return res.status(400).json({ error: JSON.parse(result.error.message) }) }
+
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) { return res.status(404).json({ message: 'Movie not found' }) }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+// Levantar el servidor http en el primer puerto disponible a partir del 1234.
+
 const PORT = process.env.PORT ?? 1234
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`)
-})
+app.listen(PORT, () => { console.log(`Server listening on port http://localhost:${PORT}`) })
