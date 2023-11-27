9bea686b758ba1daf8c9b32e77da2829f3666804
Diego Rodriguez <diego25598@gmail.com>
2023-11-27 02:36:05 +0100

    add api controllers to get movies by genre and add new movies

--- clase3/app.js
+++ clase3/app.js
@@ -1,11 +1,53 @@
 const express = require('express')
+const crypto = require('node:crypto')
+const movies = require('./movies.json')
 
 const app = express()
+app.use(express.json())
 
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Bienvenido' })
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.includes(genre)
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const {
+    title,
+    year,
+    director,
+    duration,
+    poster,
+    genre,
+    rate
+  } = req.body
+  const newMovie = {
+    id: crypto.randomUUID(),
+    title,
+    year,
+    director,
+    duration,
+    poster,
+    genre,
+    rate: rate ?? 0
+  }
+  // Esto no sería rest porque estamos guardando el estado de la aplicación en memoria.
+  movies.push(newMovie)
+  res.status(201).json(newMovie)
 })
 
 const PORT = process.env.PORT ?? 1234
