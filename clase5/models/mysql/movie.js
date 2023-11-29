6037e872579fb821ef74e16189bce796c1e3af7f
Diego Rodriguez <diego25598@gmail.com>
2023-11-29 19:25:16 +0100

    add mysql model to create movies with associated genres

--- clase5/models/mysql/movie.js
+++ clase5/models/mysql/movie.js
@@ -16,42 +16,100 @@ export class MovieModel {
     if (genre) {
       const lowerCaseGenre = genre.toLowerCase()
 
-      // get genre ids from database table using genre names
-      const [genres] = await connection.query(
-        'SELECT id, name FROM genre WHERE LOWER(name) = ?;',
-        [lowerCaseGenre]
-      )
+      // Buscar el ID del género proporcionado
+      const [genres] = await connection.query('SELECT id, name FROM genre WHERE LOWER(name) = ?;', [lowerCaseGenre])
 
-      // no genre found
+      // Si no se encuentra el género, retornar un array vacío
       if (genres.length === 0) return []
 
-      // get the id from the first genre result
+      // Obtener el ID del primer resultado de género
       const [{ id }] = genres
 
-      // get all movies ids from database table
-      // la query a movie_genres
-      // join
-      // y devolver resultados..
-      return []
+      try {
+        // Obtener todas las películas asociadas al género desde la tabla movie_genres
+        const [moviesData] = await connection.query(
+          `SELECT BIN_TO_UUID(m.id) id, m.title, m.year, m.director, m.duration, m.poster, m.rate, GROUP_CONCAT(g.name) AS genres
+            FROM movie m
+            INNER JOIN movie_genres mg ON m.id = mg.movie_id
+            INNER JOIN genre g ON mg.genre_id = g.id
+            WHERE mg.genre_id = ?
+            GROUP BY m.id;`,
+          [id]
+        )
+
+        const movies = moviesData.map(movie => ({
+          title: movie.title,
+          year: movie.year,
+          director: movie.director,
+          duration: movie.duration,
+          poster: movie.poster,
+          genre: movie.genres ? movie.genres.split(',') : [], // Verificación para asegurar que movie.genres no sea null
+          rate: movie.rate
+        }))
+
+        return movies
+      } catch (error) { throw new Error('Error fetching movies by genre') }
     }
 
-    const [movies] = await connection.query(
-      'SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id FROM movie;'
+    // Si no se proporciona ningún género, recuperar todas las películas
+
+    const [moviesData] = await connection.query(
+      `SELECT BIN_TO_UUID(m.id) id, m.title, m.year, m.director, m.duration, m.poster, m.rate, GROUP_CONCAT(g.name) AS genres
+        FROM movie m
+        LEFT JOIN movie_genres mg ON m.id = mg.movie_id
+        LEFT JOIN genre g ON mg.genre_id = g.id
+        GROUP BY m.id;`
     )
+
+    const movies = moviesData.map(movie => ({
+      // id: movie.id,
+      title: movie.title,
+      year: movie.year,
+      director: movie.director,
+      duration: movie.duration,
+      poster: movie.poster,
+      genre: movie.genres ? movie.genres.split(',') : [], // Verificación para asegurar que movie.genres no sea null
+      rate: movie.rate
+    }))
 
     return movies
   }
 
   static async getById ({ id }) {
     const [movies] = await connection.query(
-      `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id
+      `SELECT BIN_TO_UUID(id) id, title, year, director, duration, poster, rate
         FROM movie WHERE id = UUID_TO_BIN(?);`,
       [id]
     )
 
     if (movies.length === 0) return null
 
-    return movies[0]
+    const movieData = movies[0]
+
+    // Obtener los géneros asociados a la película
+    const [genresData] = await connection.query(
+      `SELECT g.name
+        FROM genre g
+        INNER JOIN movie_genres mg ON g.id = mg.genre_id
+        WHERE mg.movie_id = UUID_TO_BIN(?);`,
+      [id]
+    )
+
+    const genres = genresData.map(genre => genre.name)
+
+    // Formatear la película con los géneros obtenidos
+    const formattedMovie = {
+      // id: movieData.id,
+      title: movieData.title,
+      year: movieData.year,
+      director: movieData.director,
+      duration: movieData.duration,
+      poster: movieData.poster,
+      rate: movieData.rate,
+      genre: genres
+    }
+
+    return formattedMovie
   }
 
   static async create ({ input }) {
@@ -65,32 +123,42 @@ export class MovieModel {
       poster
     } = input
 
-    // todo: crear la conexión de genre
-
-    // crypto.randomUUID()
-    const [uuidResult] = await connection.query('SELECT UUID() uuid;')
-    const [{ uuid }] = uuidResult
+    let movieId
 
     try {
-      await connection.query(
+      // Insertar la película en la tabla movie
+      const [uuidResult] = await connection.query('SELECT UUID() uuid;')
+      const [{ uuid }] = uuidResult
+
+      const [insertMovie] = await connection.query(
         `INSERT INTO movie (id, title, year, director, duration, poster, rate)
-          VALUES (UUID_TO_BIN("${uuid}"), ?, ?, ?, ?, ?, ?);`,
+        VALUES (UUID_TO_BIN("${uuid}"), ?, ?, ?, ?, ?, ?);`,
         [title, year, director, duration, poster, rate]
       )
+
+      movieId = insertMovie.insertId // Obtener el ID de la película insertada
+
+      // Insertar los géneros asociados a la película en la tabla movie_genres
+      for (const genre of genreInput) {
+        const [genreIdResult] = await connection.query(
+          'SELECT id FROM genre WHERE name = ?;',
+          [genre]
+        )
+
+        if (genreIdResult.length > 0) {
+          const [{ id: genreId }] = genreIdResult // Obtener directamente el ID del resultado
+
+          // Insertar en movie_genres con los IDs correctos de película y género
+          await connection.query(
+            `INSERT INTO movie_genres (movie_id, genre_id) VALUES (UUID_TO_BIN("${uuid}"), ?);`,
+            [genreId]
+          )
+        }
+      }
     } catch (e) {
-      // puede enviarle información sensible
+      console.error('Error creating movie:', e) // Registrar el error detallado
       throw new Error('Error creating movie')
-      // enviar la traza a un servicio interno
-      // sendLog(e)
     }
-
-    const [movies] = await connection.query(
-      `SELECT title, year, director, duration, poster, rate, BIN_TO_UUID(id) id
-        FROM movie WHERE id = UUID_TO_BIN(?);`,
-      [uuid]
-    )
-
-    return movies[0]
   }
 
   static async delete ({ id }) {
