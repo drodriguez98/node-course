7e399e00d325c56a48202c18e7c11a7043792ef9
Diego Rodriguez <diego25598@gmail.com>
2023-11-29 19:24:12 +0100

    add script to create moviesdb database

--- clase5/models/movies.sql
+++ clase5/models/movies.sql
@@ -11,7 +11,6 @@ CREATE TABLE movie (
     director VARCHAR(255) NOT NULL, 
     duration INT NOT NULL,
     poster TEXT, 
-    genre VARCHAR(255), 
     rate DECIMAL(2,1) UNSIGNED NOT NULL
 );
 
@@ -28,20 +27,26 @@ CREATE TABLE movie_genres (
 
 INSERT INTO genre (name) 
 VALUES
-    ('Drama'),
     ('Action'),
-    ('Crime'),
     ('Adventure'),
-    ('Sci-Fi'),
-    ('Romance')
+    ('Animation'),
+    ('Biography'),
+    ('Crime'),
+    ('Romance'),
+    ('Comedy'), 
+    ('Drama'), 
+    ('Fantasy'),
+    ('Horror'), 
+    ('Thriller'), 
+    ('Sci-Fi')
 ;
 
-INSERT INTO movie (id, title, `year`, director, duration, poster, genre, rate) 
+INSERT INTO movie (id, title, `year`, director, duration, poster, rate) 
 VALUES 
-    (UUID_TO_BIN(UUID()), 'Interstellar', 1994, 'Christopher Nolan', 180, 'https://m.media-amazon.com/images/I/91obuWzA3XL._AC_UF1000,1000_QL80_.jpg', 'Sci-Fi', 8.8),
-    (UUID_TO_BIN(UUID()), 'Gladiator', 2000, 'Ridley Scott', 155, 'https://img.fruugo.com/product/0/60/14417600_max.jpg', 'Action', 8.5),
-    (UUID_TO_BIN(UUID()), 'The Lord of the Rings: The Return of the King', 2003, 'Peter Jackson', 180, 'https://i.ebayimg.com/images/g/0hoAAOSwe7peaMLW/s-l1600.jpg', 'Adventure', 8.9),
-    (UUID_TO_BIN(UUID()), 'Pulp Fiction', 1994, 'Quentin Tarantino', 180, 'https://www.themoviedb.org/t/p/original/vQWk5YBFWF4bZaofAbv0tShwBvQ.jpg', 'Crime', 8.9)
+    (UUID_TO_BIN(UUID()), 'Interstellar', 1994, 'Christopher Nolan', 180, 'https://m.media-amazon.com/images/I/91obuWzA3XL._AC_UF1000,1000_QL80_.jpg', 8.8),
+    (UUID_TO_BIN(UUID()), 'Gladiator', 2000, 'Ridley Scott', 155, 'https://img.fruugo.com/product/0/60/14417600_max.jpg', 8.5),
+    (UUID_TO_BIN(UUID()), 'The Lord of the Rings: The Return of the King', 2003, 'Peter Jackson', 180, 'https://i.ebayimg.com/images/g/0hoAAOSwe7peaMLW/s-l1600.jpg', 8.9),
+    (UUID_TO_BIN(UUID()), 'Pulp Fiction', 1994, 'Quentin Tarantino', 180, 'https://www.themoviedb.org/t/p/original/vQWk5YBFWF4bZaofAbv0tShwBvQ.jpg', 8.9)
 ;
 
 
